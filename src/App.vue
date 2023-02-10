c2ae63b1452dd39f31e0cca771a5d1dd9e0d6463
bear320 <aaabear320@gmail.com>
2023-02-11 06:28:12 +0800

    router setting

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
+        <router-link to="/">About Me</router-link> | <router-link to="/projects">Projects</router-link> |
+        <router-link to="/skills">Skills</router-link>
     </nav>
     <router-view />
 </template>
---
+++ src/views/Projects.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+    components: {},
+    data() {
+        return {
+            msg: "This is a projects page.",
+        };
+    },
+    computed: {},
+    methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
